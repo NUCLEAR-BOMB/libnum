5279a2895d67c60508825c262ad74384e70dae4e
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-04-22 19:58:43 +0500

    Add left/right rotate shift intrinsics

--- include/libnum/detail/intrinsics.hpp
+++ include/libnum/detail/intrinsics.hpp
@@ -32,4 +32,38 @@ inline std::uint64_t div128(std::uint64_t hx, std::uint64_t lx, std::uint64_t y,
     return ::_udiv128(hx, lx, y, &rem);
 }
 
+LIBNUM_FORCEINLINE
+inline std::uint64_t rotl(std::uint64_t x, std::uint64_t count) noexcept {
+    return ::_rotl64(x, count);
+}
+LIBNUM_FORCEINLINE
+inline std::uint32_t rotl(std::uint32_t x, std::uint32_t count) noexcept {
+    return ::_rotl(x, count);
+}
+LIBNUM_FORCEINLINE
+inline std::uint16_t rotl(std::uint16_t x, std::uint16_t count) noexcept {
+    return ::_rotl16(x, count);
+}
+LIBNUM_FORCEINLINE
+inline std::uint8_t rotl(std::uint8_t x, std::uint8_t count) noexcept {
+    return ::_rotl8(x, count);
+}
+
+LIBNUM_FORCEINLINE
+inline std::uint64_t rotr(std::uint64_t x, std::uint64_t count) noexcept {
+    return ::_rotr64(x, count);
+}
+LIBNUM_FORCEINLINE
+inline std::uint32_t rotr(std::uint32_t x, std::uint32_t count) noexcept {
+    return ::_rotr(x, count);
+}
+LIBNUM_FORCEINLINE
+inline std::uint16_t rotr(std::uint16_t x, std::uint16_t count) noexcept {
+    return ::_rotr16(x, count);
+}
+LIBNUM_FORCEINLINE
+inline std::uint8_t rotr(std::uint8_t x, std::uint8_t count) noexcept {
+    return ::_rotr8(x, count);
+}
+
 }
